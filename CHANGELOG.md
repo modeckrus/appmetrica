8f392674db3464a990758ab0022fc785596fa43b
modeck rus <modeckrus.official.page@gmail.com>
2022-07-07 21:11:10 +0300

    fix warning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.1.1
+
+Initial release. Supports most of the features available in AppMetrica.  
+Native SDK versions:
+- Android: 5.0.0
+- iOS: 4.2
 ## 0.1.0
 
 Initial release. Supports most of the features available in AppMetrica.  
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<appmetrica/YMMFAppMetricaPlugin.h>)
+#import <appmetrica/YMMFAppMetricaPlugin.h>
+#else
+@import appmetrica;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [YMMFAppMetricaPlugin registerWithRegistrar:[registry registrarForPlugin:@"YMMFAppMetricaPlugin"]];
+}
+
+@end
